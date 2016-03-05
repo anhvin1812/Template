8a2394b2ef31963a7157d7eed20df7d14894934d
Tuan Le Manh <leanhvin@hotmail.com>
2016-03-05 15:09:06 +0700

    create

---
+++ WebApp/Html/js/common.js
@@ -0,0 +1,11 @@
+jQuery(function() {
+    $('.page-slider').bxSlider({
+        touchEnabled: false,
+        mode: 'fade',
+        auto: true,
+        speed: 600,
+        tickerHover: true,
+        pager: false,
+    });
+
+});
