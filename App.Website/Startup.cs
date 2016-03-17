69ba8c7f59e3238c92d82e1b591066c4686804eb
Tuan Le <Tuan.M.Le@SherpaSystems.com.au>
2016-03-17 13:08:45 +0700

    Update Solution

---
+++ App.Website/Startup.cs
@@ -0,0 +1,15 @@
+﻿using App.Website;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace App.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
