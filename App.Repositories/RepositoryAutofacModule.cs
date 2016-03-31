9136c8d95b9aad9f5d966bf5592a34f73adac463
Tuan Le <Tuan.M.Le@SherpaSystems.com.au>
2016-03-31 13:47:32 +0700

    include missed files

---
+++ App.Repositories/RepositoryAutofacModule.cs
@@ -0,0 +1,20 @@
+﻿using App.Data.EntityFramework;
+using App.Repositories.IdentityManagement;
+using Autofac;
+
+namespace App.Repositories
+{
+    public class RepositoryAutofacModule: Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            // User
+            builder.RegisterType<UserRepository>().As<IUserRepository>().InstancePerDependency();
+            //builder.RegisterType<SherpaRosterRepository>().Keyed<IRosterExtendedRepository>(DatabaseInstance.SherpaRoster).InstancePerDependency();
+            //builder.RegisterType<GCrewRosterRepository>().Keyed<IRosterExtendedRepository>(DatabaseInstance.GCrewRoster).InstancePerDependency();
+            
+            builder.RegisterModule(new DataEntityFrameworkAutofacModule());
+        }
+    }
+}
