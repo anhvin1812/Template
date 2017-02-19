2ff6421ec622b7c52fe00c83d3d27f101a1d5e47
Tuan Le Manh <leanhvin@hotmail.com>
2017-02-20 00:09:37 +0700

    - create product

---
+++ App.Services.Dtos/ProductManagement/ProductStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using App.Services.Dtos.Common;
+
+namespace App.Services.Dtos.ProductManagement
+{
+    public class ProductStatusSummary : DtoBase
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ App.Services.Dtos/UI/SelectListOptions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using App.Services.Dtos.Common;
+
+namespace App.Services.Dtos.UI
+{
+    public class SelectListOptions : DtoBase
+    {
+        public IEnumerable Items { get; set; } 
+        public IEnumerable DisabledValues { get; set; }
+        public string DataTextField { get; set; }
+        public string DataValueField { get; set; }
+    }
+}
