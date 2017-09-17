d8ca39c380a78ee63d2c3876be7cff775d0d2329
Tuan Le Manh <leanhvin@hotmail.com>
2017-09-17 13:17:47 +0700

    - elmah log 2

---
+++ App.Website/Areas/Admin/Controllers/ElmahController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using App.Services;
+using App.Services.Dtos.Common;
+using App.Website.Fillters;
+
+namespace App.Website.Areas.Admin.Controllers
+{
+    [ServiceAuthorization(Roles = "Administrator")]
+    public class ElmahController : BaseController
+    {
+        public ElmahController()
+            : base(new IService[] {  })
+        {
+        }
+
+        public ActionResult Index(string type)
+        {
+            return new ElmahResult(type);
+        }
+      
+    }
+    
+}
